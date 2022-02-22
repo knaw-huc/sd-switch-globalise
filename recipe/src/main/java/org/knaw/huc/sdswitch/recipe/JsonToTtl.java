4fda0d62386bcb233013d4042246e14eb40475b0
meindertkroese <>
2022-02-22 16:34:36 +0100

    building ttl output

--- recipe/src/main/java/org/knaw/huc/sdswitch/recipe/JsonToTtl.java
+++ recipe/src/main/java/org/knaw/huc/sdswitch/recipe/JsonToTtl.java
@@ -1,15 +1,60 @@
 package org.knaw.huc.sdswitch.recipe;
 
+import org.json.JSONException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.XMLConstants;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+
+
 public class JsonToTtl {
 
-  public static String jsonToTtl(String json) {
-    String ttl;
-    ttl = convert(json);
+  static String rdfSubject = "";
+  static String rdfType = "";
+
+  public static String jsonToTtl(String json) throws JSONException {
+    Document schema = readSchema();
+    JSONObject jsonObject = new JSONObject(json);
+
+    //getting first and last name
+    String id = jsonObject.getString("id");
+    rdfSubject.replace("id",id);
+    String ttl = rdfSubject + " a " + rdfType + ".";
     return ttl;
   }
 
-  private String convert(String textIn) {
-    String textOut = textIn;
-    return textOut;
+  public static Document readSchema() {
+    String filename = "src/main/resources/schema_raa.xml";
+    DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+    Document doc = null;
+
+    try {
+      dbf.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+      DocumentBuilder db = dbf.newDocumentBuilder();
+      doc = db.parse(new File(filename));
+      NodeList list = doc.getElementsByTagName("persoon");
+      Node node = list.item(0);
+      if (node.getNodeType() == Node.ELEMENT_NODE) {
+        Element element = (Element) node;
+        rdfSubject = element.getAttribute("rdf:subject");
+        rdfType = element.getAttribute("rdf:type");
+      }
+    } catch (ParserConfigurationException | SAXException | IOException e) {
+      e.printStackTrace();
+    }
+    return doc;
   }
 }
--- recipe/src/test/java/org/knaw/huc/sdswitch/recipe/JsonToTtlTest.java
+++ recipe/src/test/java/org/knaw/huc/sdswitch/recipe/JsonToTtlTest.java
@@ -1,15 +1,37 @@
 package org.knaw.huc.sdswitch.recipe;
 
+import org.json.JSONException;
 import org.junit.Assert;
 import org.junit.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 public class JsonToTtlTest {
 
   @Test
-  void runTestJsonTtl() throws Exception {
+  public void runTestJsonTtl() throws JSONException {
     String json = "{ \"id\":1 }";
     String expectedResult = "<https://humanities.knaw.nl/raa/person/1> a <https://humanities.knaw.nl/person>.";
     String result = JsonToTtl.jsonToTtl(json);
-    Assert.assertEquals(expectedResult,result);
+    Assert.assertEquals(expectedResult, result);
+  }
+
+  @Test
+  public void runTestSchema() {
+    Document doc = JsonToTtl.readSchema();
+    System.out.println("Root Element :" + doc.getDocumentElement().getNodeName());
+    String result = doc.getDocumentElement().getNodeName();
+    Assert.assertEquals("s:S", result);
+    // NodeList list = doc.getElementsByTagName("persoon");
+    // Node node = list.item(0);
+    // if (node.getNodeType() == Node.ELEMENT_NODE) {
+    //   Element element = (Element) node;
+    //   String rdfSubject = element.getAttribute("rdf:subject");
+    //   String rdfType = element.getAttribute("rdf:type");
+      Assert.assertEquals("https://humanities.knaw.nl/raa/person/{id}", JsonToTtl.rdfSubject);
+      Assert.assertEquals("https://humanities.knaw.nl/person", JsonToTtl.rdfType);
+    // }
   }
 }
