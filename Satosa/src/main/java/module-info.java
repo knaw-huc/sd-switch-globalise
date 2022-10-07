8bb6d3af0039d0fdcb2dbb54dca980bb9fac75e2
kerim1 <kerim.meijer@di.huc.knaw.nl>
2022-10-07 12:26:51 +0200

    Work in progress

--- Satosa/src/main/java/module-info.java
+++ Satosa/src/main/java/module-info.java
@@ -1,5 +1,7 @@
 module org.knaw.huc.sdswitch.satosa {
     requires Saxon.HE;
+    requires SaxonUtils;
+    requires java.ws.rs;
     requires org.knaw.huc.sdswitch.recipe;
     requires org.knaw.huc.auth;
     provides org.knaw.huc.sdswitch.recipe.Recipe with org.knaw.huc.sdswitch.satosa.SatosaProxyRecipe;
--- Satosa/src/main/java/org/knaw/huc/sdswitch/satosa/SatosaProxyRecipe.java
+++ Satosa/src/main/java/org/knaw/huc/sdswitch/satosa/SatosaProxyRecipe.java
@@ -2,11 +2,16 @@ package org.knaw.huc.sdswitch.satosa;
 
 import net.sf.saxon.s9api.XdmItem;
 import org.knaw.huc.auth.OpenID;
+import org.knaw.huc.auth.data.Essential;
 import org.knaw.huc.sdswitch.recipe.Recipe;
 import org.knaw.huc.sdswitch.recipe.RecipeData;
 import org.knaw.huc.sdswitch.recipe.RecipeParseException;
 import org.knaw.huc.sdswitch.recipe.RecipeResponse;
 
+import javax.ws.rs.core.UriBuilder;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
 
@@ -16,7 +21,25 @@ public class SatosaProxyRecipe implements Recipe<SatosaProxyRecipe.SatosaProxyCo
 
     @Override
     public SatosaProxyConfig parseConfig(XdmItem config, Set<String> pathParams) throws RecipeParseException {
-        return null;
+        final URI oidcServer = URI.create(System.getenv().get("OIDC_SERVER"));
+        final URI applicationUrl = URI.create(System.getenv().getOrDefault("APPLICATION_URL", "http://localhost"));
+        final URI redirectUrl = UriBuilder.fromUri(applicationUrl).path("/redirect").build();
+
+        final String clientId = System.getenv().get("OIDC_CLIENT_ID");
+        final String clientSecret = System.getenv().get("OIDC_CLIENT_SECRET");
+
+        Map<String, Essential> userInfoClaims = new HashMap<>();
+        userInfoClaims.put("edupersontargetedid", null);
+        userInfoClaims.put("schac_home_organisation", null);
+        userInfoClaims.put("nickname", null);
+        userInfoClaims.put("email", null);
+        userInfoClaims.put("eppn", null);
+        userInfoClaims.put("idp", null);
+
+        OpenID openID = new OpenID(oidcServer, redirectUrl, clientId, clientSecret,
+                Map.of("userinfo", userInfoClaims), "openid", "email", "profile");
+
+        return new SatosaProxyConfig(openID);
     }
 
     @Override
