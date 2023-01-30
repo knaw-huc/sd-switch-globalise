e3f61451e428ea048a0fede58e0c95b9a7a570a5
kerim1 <kerim.meijer@di.huc.knaw.nl>
2023-01-30 15:53:25 +0100

    Test with other OpenID URLs

--- auth/src/main/java/org/knaw/huc/auth/data/Tokens.java
+++ auth/src/main/java/org/knaw/huc/auth/data/Tokens.java
@@ -1,12 +1,14 @@
 package org.knaw.huc.auth.data;
 
+import com.fasterxml.jackson.annotation.JsonAnySetter;
 import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.Map;
 
 public record Tokens(@JsonProperty("access_token") String accessToken,
                      @JsonProperty("token_type") String tokenType,
                      @JsonProperty("refresh_token") String refreshToken,
                      @JsonProperty("expires_in") long expiresIn,
-                     @JsonProperty("refresh_expires_in") long refreshExpireIn,
-                     @JsonProperty("not-before-policy") String notBeforePolicy,
-                     @JsonProperty("id_token") String idToken) {
+                     @JsonProperty("id_token") String idToken,
+                     @JsonAnySetter Map<String, String> extra) {
 }
