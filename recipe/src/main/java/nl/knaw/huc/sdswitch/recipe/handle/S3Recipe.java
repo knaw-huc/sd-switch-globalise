7a0af2213ca7feeedb5a86d01e02293947460946
Meindert Kroese <meindertkroese@mac.home>
2025-09-24 15:54:24 +0200

    return body value

--- recipe/src/main/java/nl/knaw/huc/sdswitch/recipe/handle/S3Recipe.java
+++ recipe/src/main/java/nl/knaw/huc/sdswitch/recipe/handle/S3Recipe.java
@@ -6,6 +6,12 @@ import nl.knaw.huc.sdswitch.recipe.Recipe;
 import nl.knaw.huc.sdswitch.recipe.RecipeData;
 import nl.knaw.huc.sdswitch.recipe.RecipeResponse;
 import nl.knaw.huc.sdswitch.recipe.RecipeValidationException;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.Set;
 
 public class S3Recipe implements Recipe<Void> {
@@ -22,6 +28,21 @@ public class S3Recipe implements Recipe<Void> {
         String prefix = data.pathParam("prefix");
         String uuid = data.pathParam("uuid");
         String url = "https://globalise.huygens.knaw.nl/#" + "/" + uuid + "?noredirect";
-        return RecipeResponse.withRedirect(url, 301);
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .build();
+        HttpResponse<String> response = null;
+        try {
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        String body = response.body();
+//        return RecipeResponse.withRedirect(url, 301);
+        String contentType = "application/json";
+        return RecipeResponse.withBody(body, contentType);
     }
 }
